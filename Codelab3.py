fa66959bee86724899ed581b5868d3274d7f32d9
Kech <77866313+oawujo@users.noreply.github.com>
2022-11-04 18:37:10 +0000

    Add files via upload

---
+++ Codelab3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  4 18:33:12 2022
+
+@author: oawujo
+"""
+x = float(input("Enter value for x: "))
+
+# Write your code here.
+
+
+print("y =", y)
+
